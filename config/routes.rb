677b6302ded6461eb00630efb4528fb6cfd314af
voidzero <kartikjagdale11@gmail.com>
2016-09-22 16:49:13 +0530

    1. Add Versioning to Api 2. Add Routes for Api 3. Add Api Constarint to render default api version

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+require 'api_constraint'
 Rails.application.routes.draw do
-  resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # => For details on the DSL available within this file, 
+  # => see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: 'json'} do
+    scope module: :v0, constraints: ApiConstraint.new(version: 0, default: true) do
+      resources :posts
+    end
+  end
 end
